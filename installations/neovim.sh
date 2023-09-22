fef5abb721ceb24b538afab600685da146009717
sg <sg@nuc12.attlocal.net>
2023-09-22 14:52:25 -0700

    add autoinstallation of neovim

---
+++ installations/neovim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# install the latest stable neovim
+
+sudo apt remove neovim -y && \
+sudo add-apt-repository ppa:neovim-ppa/stable && \
+sudo apt-get update && \
+sudo apt-get install neovim
